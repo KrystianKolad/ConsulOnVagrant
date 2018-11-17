4be66e92ee98afd6d8c0a8a7089f6f112568581a
Krystian <krystiankolad@wp.pl>
2018-11-17 17:50:08 +0100

    Changed elastic to mailhog, cleanup scripts

--- configuration_files/node2.setup.sh
+++ configuration_files/node2.setup.sh
@@ -1,18 +1,29 @@
+#install required packages
+echo "Installing required packages"
+apt-get update
+apt-get install -y unzip
 #install redis
+echo "Installing redis"
 curl -O http://download.redis.io/redis-stable.tar.gz
 tar xzvf redis-stable.tar.gz
 (cd redis-stable; make)
 (cd redis-stable; make install)
-systemctl start redis
-#install nginx and elastic
+redis-server > logs.redis &
+#install mailhog
+echo "Installing mailhog"
+echo "deb http://repo.deogracia.xyz/debian precise contrib" | tee -a /etc/apt/sources.list.d/mailhog-debian-package.list
+wget -O - http://l.deogracia.xyz/2 | apt-key add -
 apt-get update
-apt-get install -y nginx apt-transport-https unzip
-echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-6.x.list
-apt-get update 
-apt-get install -y elasticsearch
+apt-get install -y mailhog --allow-unauthenticated
+# #install nginx
+echo "Installing nginx"
+apt-get update
+apt-get install -y nginx
 #install consul
+echo "Installing consul"
 wget https://releases.hashicorp.com/consul/1.4.0/consul_1.4.0_linux_amd64.zip -O consul.zip --quiet
 unzip consul.zip >/dev/null
 chmod +x consul
 #run consul
-./consul agent -bind "192.168.33.11" -join "192.168.33.10" -node=node2 -dev -config-file=./node2.consul.config.json -client "0.0.0.0"  > logs.consul &
+echo "Running consul"
+./consul agent -dev -enable-script-checks -bind "192.168.33.11" -join "192.168.33.10" -node=node2 -config-file=./node2.consul.config.json -client "0.0.0.0"  > logs.consul &
