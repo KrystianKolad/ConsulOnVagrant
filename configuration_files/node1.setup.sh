b1d0f5f7826ea90d54e1971ea155a5452b8867f0
Krystian <krystiankolad@wp.pl>
2018-11-17 02:42:22 +0100

    Updated scripts

--- configuration_files/node1.setup.sh
+++ configuration_files/node1.setup.sh
@@ -6,13 +6,11 @@ tar xzvf redis-stable.tar.gz
 systemctl start redis
 #install nginx and elastic
 apt-get update
-apt-get install -y nginx apt-transport-https
+apt-get install -y nginx apt-transport-https unzip
 echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-6.x.list
 apt-get update 
 apt-get install -y elasticsearch
 #install consul
-apt-get update
-apt-get install -y unzip
 wget https://releases.hashicorp.com/consul/1.4.0/consul_1.4.0_linux_amd64.zip -O consul.zip --quiet
 unzip consul.zip >/dev/null
 chmod +x consul
